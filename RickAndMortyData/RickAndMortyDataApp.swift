b68d1acf35eb7850714c690abd5235a7424f16ec
amolon615 <114776102+amolon615@users.noreply.github.com>
2022-12-03 19:30:35 +0100

    Initial Commit

---
+++ RickAndMortyData/RickAndMortyDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyDataApp.swift
+//  RickAndMortyData
+//
+//  Created by Artem on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
